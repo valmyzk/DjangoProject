13aa0ed2ab7d6ee1bd063e2c14da1125d3cfb19a
oleksandr <oleksandr.mudryk01@gmail.com>
2025-05-08 17:38:20 +0200

    reorganized templates, added new templates, improved the base template adding the remaining buttons, and added the corresponding urls.

--- WebProject/urls.py
+++ WebProject/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.root, name='root'),
+    path('assets/buy/', views.buy, name='buy'),
+    path('assets/sell/', views.sell, name='sell'),
+    path('funds/add/', views.add_funds, name='add_funds'),
+    path('funds/withdraw/', views.transfer_funds, name='transfer_funds'),
+    path('profile', views.my_profile, name='my_profile')
 ]
--- WebProject/views.py
+++ WebProject/views.py
@@ -7,3 +7,18 @@ def root(request: HttpRequest) -> HttpResponse:
     if request.user.is_authenticated:
         return render(request, 'dashboard.html')
     return render(request, 'home.html')
+
+def buy(request: HttpRequest) -> HttpResponse:
+    return render(request, 'buy.html')
+
+def sell(request: HttpRequest) -> HttpResponse:
+    return render(request, 'sell.html')
+
+def add_funds(request: HttpRequest) -> HttpResponse:
+    return render(request, 'add_funds.html')
+
+def transfer_funds(request: HttpRequest) -> HttpResponse:
+    return render(request, 'transfer_funds.html')
+
+def my_profile(request: HttpRequest) -> HttpResponse:
+    return render(request, 'my_profile.html')
