42af22f02d12b197d6cc87a7d6e74b54ff9bf3a9
Eric Gil <egc20@alumnes.udl.cat>
2025-05-09 12:10:33 +0200

    feat: add edit profile functionality and corresponding templates

--- WebProject/urls.py
+++ WebProject/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('assets/sell/', views.sell, name='sell'),
     path('funds/add/', views.add_funds, name='add_funds'),
     path('funds/withdraw/', views.transfer_funds, name='transfer_funds'),
-    path('profile', views.my_profile, name='my_profile')
+    path('profile', views.my_profile, name='my_profile'),
+    path('profile/edit', views.edit_profile, name='edit_profile'),
 ]
