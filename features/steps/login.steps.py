167a5eee5cad9dc34fa179e4c0858b4183702f46
Valentí Martín <30903958+valmyzk@users.noreply.github.com>
2025-05-13 23:40:35 +0200

    fix: login test

--- features/steps/login.steps.py
+++ features/steps/login.steps.py
@@ -1,13 +1,14 @@
 from behave import given, when, then
 from django.contrib.auth import get_user_model
 
+from datetime import date
 
 User = get_user_model()
 
 @given('a user exists with email "{email}" and password "{password}"')
 def step_impl(context, email, password):
     if not User.objects.filter(email=email).exists():
-        User.objects.create_user(email=email, password=password)
+        User.objects.create_user(email=email, password=password, phone='+34 123 456 789', date_of_birth=date(1970, 1, 1))
 
 @given('I am on the login page')
 def step_impl(context):
