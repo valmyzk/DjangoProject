37620c1ab93bc684a7e7b57f0a6edbf84bad3267
gilito11 <egc20@alumnes.udl.cat>
2025-05-23 18:49:43 +0200

    feat: update transfer detail view and add asset chart to wallet

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -118,11 +118,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'  # o la que necesites
+USE_TZ = True
 
 USE_I18N = True
-
-USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
